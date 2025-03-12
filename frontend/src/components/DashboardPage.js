33ca788e535c991ad3c324e049f1d8226e1050dd
dikurdikur <dimaskur1533@gmail.com>
2025-03-12 23:34:42 +0800

    Create DashboardPage.js
    Komponen React ini menampilkan halaman dashboard dengan ringkasan tugas, XP, dan streaks.

---
+++ frontend/src/components/DashboardPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function DashboardPage() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <p>Summary of Pending Tasks, XP, and Streaks.</p>
+    </div>
+  );
+}
+
+export default DashboardPage;
